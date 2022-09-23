d047c2cc0677ca298e7607d4e43ce54d3a802fda
Felipe <lionffl@gmail.com>
2022-09-23 17:51:32 -0300

    User Model added

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false, 
+    },
+    displayName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    }
+  }, {
+    timestamps: false,
+    underscored: true,
+  })
+  return User;
+};
+
+module.exports = UserModel;
