8f5ce5298e8d27592bd115d0ff2c25a6ad1994e9
Felipe <lionffl@gmail.com>
2022-09-23 17:30:30 -0300

    migrations for users, categories, blog_posts and posts_categories tables added

---
+++ src/migrations/20220923200150-create-blog_posts.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('blog_posts', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false, 
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      published: {
+        type: Sequelize.DATE,
+      },
+      updated: {
+        type: Sequelize.DATE,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.dropTable('blog_posts');
+  }
+};
