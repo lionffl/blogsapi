138699c34d0a5dd7f65f89b871f73258476c4e26
Felipe <lionffl@gmail.com>
2022-09-26 10:52:22 -0300

    refactor: login controller and loginFieldsValidation middleware

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -5,12 +5,9 @@ module.exports.login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const login = await LoginService.login(email, password);
-    if (login.length === 0) res.status(400).json({ message: 'Invalid fields' });
-    else {
-      const { id } = login[0].dataValues;
-      const token = getToken(email, id);
-      res.status(200).json({ token }); 
-}
+    const { id } = login[0].dataValues;
+    const token = getToken(email, id);
+    res.status(200).json({ token });
   } catch (error) {
     console.error(error.message);
     res.status(400).json({ message: 'Something is wrong.' });
--- src/middlewares/loginFieldsValidation.js
+++ src/middlewares/loginFieldsValidation.js
@@ -1,5 +1,16 @@
-module.exports.loginFieldsValidation = (req, res, next) => {
+const LoginService = require('../services/login.service');
+
+const errorEmptyFieldsMsg = { message: 'Some required fields are missing' };
+const invalidFieldsMsg = { message: 'Invalid fields' };
+
+module.exports.loginFieldsValidation = async (req, res, next) => {
   const { email, password } = req.body;
-  if (!email || !password) res.status(400).json({ message: 'Some required fields are missing' });
-  else next();
+  
+  if (!email || !password) return res.status(400).json(errorEmptyFieldsMsg);
+  
+  const login = await LoginService.login(email, password);
+  
+  if (login.length === 0) return res.status(400).json(invalidFieldsMsg);
+  
+  next();
 };
